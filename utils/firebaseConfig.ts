a1c9b784c25d9240cde3a18a310b85a5c8f3cd60
rockreyad <rockreyad@gmail.com>
2023-09-06 00:19:30 +0600

    firebase config

---
+++ utils/firebaseConfig.ts
@@ -0,0 +1,21 @@
+import { initializeApp, getApps } from "firebase/app";
+import { getDatabase } from "firebase/database";
+
+const firebaseConfig = {
+  apiKey: process.env.NEXT_PUBLIC_FIREBASE_API_KEY,
+  authDomain: process.env.NEXT_PUBLIC_FIREBASE_AUTH_DOMAIN,
+  projectId: process.env.NEXT_PUBLIC_FIREBASE_PROJECT_ID,
+  storageBucket: process.env.NEXT_PUBLIC_FIREBASE_STORAGE_BUCKET,
+  messagingSenderId: process.env.NEXT_PUBLIC_FIREBASE_MESSAGING_SENDER_ID,
+  appId: process.env.NEXT_PUBLIC_FIREBASE_APP_ID,
+  measurementId: process.env.NEXT_PUBLIC_FIREBASE_MEASUREMENT_ID,
+  databaseURL: process.env.NEXT_PUBLIC_FIREBASE_DATABASE_URL,
+};
+
+// Initialize Firebase
+let firebase_app = !getApps().length
+  ? initializeApp(firebaseConfig)
+  : getApps()[0];
+
+export const db = getDatabase(firebase_app);
+export default firebase_app;
